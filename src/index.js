dc6dbb90a706b8006f40cbf4c49bd8f11e8b7862
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-04-04 14:17:57 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
-}
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const movieId = parseInt(
+            document.getElementById('searchByID').value,
+            10
+        );
+
+        fetch(`http://localhost:3000/movies/${movieId}`)
+            .then((resp) => resp.json())
+            .then((data) => {
+                const movieTitleEl = document.querySelector('#movieDetails h4');
+                const movieSummaryEl =
+                    document.querySelector('#movieDetails p');
+
+                movieTitleEl.textContent = `Title: ${data.title}`;
+                movieSummaryEl.textContent = `Summary: ${data.summary}`;
+            });
+    });
+};
 
 document.addEventListener('DOMContentLoaded', init);
